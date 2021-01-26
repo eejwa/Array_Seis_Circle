509bd2049dbd40eb34e5367ef61d04945745e276
eejwa <eejwa@leeds.ac.uk>
2021-01-26 15:03:45 +0000

    adds test to plot traces against backazimuth as well as distance

--- tests/record_section_plotting.py
+++ tests/record_section_plotting.py
@@ -5,14 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from array_plotting import plotting
 
-st = obspy.read('./data/19990405/*SAC')
+st = obspy.read('./data/19970525/*SAC')
 
 
-fig = plt.figure(figsize=(8,8))
+fig = plt.figure(figsize=(14,8))
 ax = fig.add_subplot(121)
 p = plotting(ax)
-p.plot_record_section_SAC(st=st, phase="SKS", tmin=20, tmax=120, align=False)
+p.plot_record_section_SAC(st=st, phase="SKS", tmin=20, tmax=120, align=False, type='baz')
 ax = fig.add_subplot(122)
 p = plotting(ax)
-p.plot_record_section_SAC(st=st, phase="SKS", tmin=20, tmax=120, align=True)
+p.plot_record_section_SAC(st=st, phase="SKS", tmin=20, tmax=120, align=True, type='baz')
 plt.show()
